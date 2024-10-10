4d7e2567e2727c4efb06e3cebb1e739b9d29bd62
Rafafi123 <gonzalesrafael93@gmail.com>
2024-10-10 12:02:04 +0800

    Database set

--- SIMS/settings.py
+++ SIMS/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'SIMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'simsdb',
+        'USER': 'postgres',
+        'PASSWORD': 'SmartPostGreMan_97',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
