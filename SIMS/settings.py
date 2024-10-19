dbc37bb71fb2572d5a781f112289698e1140b6ce
Rafafi123 <gonzalesrafael93@gmail.com>
2024-10-19 20:29:35 +0800

    student templates with settings config adjusted

--- SIMS/settings.py
+++ SIMS/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'students.apps.StudentsConfig',
+    'classroom.apps.ClassroomsConfig',
+    'teachers.apps.TeachersConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
