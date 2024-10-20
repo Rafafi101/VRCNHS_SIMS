838dca5b2eb60c1e07846be8ee6aa5a9f13e73f9
Rafafi123 <gonzalesrafael93@gmail.com>
2024-10-20 23:51:32 +0800

    dataTables works with main.js student template fixed

--- SIMS/static/js/main.js
+++ SIMS/static/js/main.js
@@ -4,3 +4,18 @@ document.querySelector('.year').innerHTML = date.getFullYear();
 setTimeout(function() {
     $('#message').fadeOut('slow');
 }, 5000);
+
+// Add jQuery and DataTables scripts to main.js
+$.getScript("https://code.jquery.com/jquery-3.6.4.min.js", function() {
+    $.getScript("https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js", function() {
+        $(document).ready(function () {
+            $('#studentTable').DataTable({
+                dom: 'lrtip', // Only show the length changing, processing, and table parts
+                paging: true, // Enable pagination
+                searching: true, // Enable searching
+                ordering: true, // Enable column ordering
+                info: true // Show table information
+            });
+        });
+    });
+});
