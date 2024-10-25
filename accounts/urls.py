5af3f687fa9937e9037967e347515604d516d79f
Rafafi123 <gonzalesrafael93@gmail.com>
2024-10-26 01:00:30 +0800

    password changing is functionall

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 
@@ -8,4 +8,9 @@ urlpatterns = [
     path('register', views.teacher_register, name='register'),
     path('logout', views.logout, name='logout'),
     path('captcha/', include('captcha.urls')),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='accounts/password_change.html'),
+         name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_changed.html'),
+         name='password_change_done'),
+
 ]
