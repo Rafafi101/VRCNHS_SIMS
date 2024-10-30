394a3388ccbb757aa1a954a92a294658fb08ac6e
Rafafi123 <gonzalesrafael93@gmail.com>
2024-10-31 03:31:08 +0800

    imports removed

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,7 +1,4 @@
 from django.apps import AppConfig
-from django.contrib.auth.models import Group, User
-from django.conf import settings
-from django.db.models.signals import post_migrate
 
 
 class AccountsConfig(AppConfig):
