4cfcb139cbf27649637af50bbb5b887135a2105c
Rafafi123 <gonzalesrafael93@gmail.com>
2024-10-30 23:49:30 +0800

    groups configuration upon startup

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,4 +1,7 @@
 from django.apps import AppConfig
+from django.contrib.auth.models import Group, User
+from django.conf import settings
+from django.db.models.signals import post_migrate
 
 
 class AccountsConfig(AppConfig):
@@ -6,4 +9,8 @@ class AccountsConfig(AppConfig):
     name = 'accounts'
 
     def ready(self):
-        import accounts.signals
+        import accounts.signals  # Import signals to ensure they are registered
+
+        # Create "ADMIN" and "TEACHER" groups when the app starts
+        Group.objects.get_or_create(name='ADMIN')
+        Group.objects.get_or_create(name='TEACHER')
